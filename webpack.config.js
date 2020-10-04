41f0158610d29121c85b5f9bc4a40682db968d41
kadonomaro <denis.afer@gmail.com>
2020-10-04 15:24:18 +0700

    init: add initial project files

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/js/main.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/js'),
+	},
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        }
+    },
+    optimization: {
+        minimizer: [new TerserJSPlugin(), new OptimizeCSSAssetsPlugin()],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/style.css',
+        }),
+        new CleanWebpackPlugin()
+      ],
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+			{
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+        ],
+    },
+};
