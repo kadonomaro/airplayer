1f207caebb832af56436644c5719367baee18fec
Kadonomaro <denis.afer@gmail.com>
2020-10-19 22:39:26 +0700

    feat: add EventObservers class

---
+++ src/libs/EventObserver.ts
@@ -0,0 +1,18 @@
+export default class EventObserver {
+	private observers: Array<Function>;
+	constructor() {
+		this.observers = [];
+	}
+
+	subscribe(fn: Function): void {
+		this.observers.push(fn);
+	}
+
+	unsubscribe(fn: Function): void {
+		this.observers = this.observers.filter(subscriber => subscriber !== fn);
+	}
+
+	broadcast(data: any) {
+		this.observers.forEach(subscriber => subscriber(data));
+	}
+}
